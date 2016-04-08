8097d388297e9874915895d11a6dc0dddf908c9b
rosshendry <ross_hendry@live.co.uk>
2016-04-08 01:30:27 +0100

    RH160408/1 - Geolocation functionality add + time functionality add + home page portal add + assorted style changes

--- build/www/js/controllers.js
+++ build/www/js/controllers.js
@@ -1,35 +1,74 @@
 app.controller('homeCtrl', function($scope, $ionicSideMenuDelegate){
-  $scope.openMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+	$scope.openMenu = function () {
+		$ionicSideMenuDelegate.toggleLeft();
+	};
+
+	/* Clock */
+	var time = document.getElementById('home-time'),
+		date = document.getElementById('home-date')
+	        dateObj = null;
+
+	var update = function () {
+		time = document.getElementById('home-time');
+		date = document.getElementById('home-date');
+	    dateObj = moment(new Date());
+	    time.innerHTML = dateObj.format('h:mm A');
+	    date.innerHTML = dateObj.format('dddd, MMMM Do YYYY');
+	};
+
+	/* Init functions */
+    update();
+    setInterval(update, 1000 * 10);
 })
 
 app.controller('faqCtrl', function($scope, $ionicSideMenuDelegate){
-  $scope.openMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+	$scope.openMenu = function () {
+		$ionicSideMenuDelegate.toggleLeft();
+	};
 })
 
-app.controller('mapCtrl', function($scope){
-	// onSuccess Callback
-	//   This method accepts a `Position` object, which contains
-	//   the current GPS coordinates
-	//
-	function onSuccess(position) {
-	    var element = document.getElementById('geolocation');
-	    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-	                        'Longitude: ' + position.coords.longitude     + '<br />' +
-	                        '<hr />'      + element.innerHTML;
-	}
+app.controller('mapCtrl', function($scope, $state, $cordovaGeolocation){
+	/* The options for our map */
+	var posOptions = {
+		enableHighAccuracy: true,
+		timeout: 10000,
+		maximumAge: 20000
+	};
 
-	// onError Callback receives a PositionError object
-	//
-	function onError(error) {
+	$cordovaGeolocation.getCurrentPosition(posOptions).then(function(position){
+		/* Identify our canvas and placeholder */
+		var canvas = document.getElementById('map-canvas');
+	    var placeholder = document.getElementById('placeholder');
+	    if(placeholder != null) placeholder.remove();
+
+	    /* Create our map element and output it */
+	    var m = document.createElement("DIV");
+		m.id = "map";	    
+
+	    var att = document.createAttribute("data-tap-disabled");
+	    att.value = "true";
+	    m.setAttributeNode(att);
+	    canvas.innerHTML = canvas.innerHTML + m.outerHTML;
+
+	    /* Position data setup and output */
+	    var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
+
+	    var myLatLng = new google.maps.LatLng(lat, lng);
+
+        var mapOptions = {
+            center: myLatLng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true
+        };
+
+	    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		var marker = new google.maps.Marker({position: myLatLng, map: map, title: 'Current Position', animation: google.maps.Animation.DROP});
+
+	    $scope.map = map;
+	}, function(error){
 	    alert('code: '    + error.code    + '\n' +
 	          'message: ' + error.message + '\n');
-	}
-
-	// Options: throw an error if no update is received every 30 seconds.
-	//
-	var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+	});
 })
